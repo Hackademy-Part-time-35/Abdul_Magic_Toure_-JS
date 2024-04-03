92604664683d43e55ba0c04764c4b8ef86b0499e
magic.toure <magictoureabdul@gmail.com>
2024-04-03 20:35:07 +0200

    Esercitazioni in Js di Abdul Magic Toure v1.0

---
+++ Esercitazione 03-04-2024/file JS.js
@@ -0,0 +1,21 @@
+let num1 = 0;
+
+let num2 = 1;
+
+console.log("Il valore della variabile num1 è: "+ num1);
+
+console.log("Il valore della variabile num1 è: "+ num2);
+
+// let stringa = "Magic";
+
+// console.log("Il valore della variabile stringa è: "+stringa);
+
+let stringa = "Valore sostituito";
+
+console.log("Il valore della variabile stringa è: "+ stringa);
+
+const PIGRECO = 3.14;
+
+console.log("Il valore di PIGRECO è: "+ PIGRECO);
+
+// let PIGRECO = 4;
