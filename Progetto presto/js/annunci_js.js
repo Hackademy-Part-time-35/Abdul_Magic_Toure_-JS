8e4801926499baf6e385787d2ec9e96de1264aa5
magic.toure <magictoureabdul@gmail.com>
2024-05-14 21:02:04 +0200

    Progetto Presto.it V6

--- Progetto presto/js/annunci_js.js
+++ Progetto presto/js/annunci_js.js
@@ -16,42 +16,73 @@
 
 let cont_annunci =  document.querySelector("#cont_annunci");
 
+let contenitore_radio = document.querySelector("#contenitore_radio");
+
 fetch("./dati/elenco_immobili.json")
 .then(response => response.json())
 .then(data => {
+  
+  function creaCardAnnunci() {
     
-    function creaCardAnnunci() {
-
-        data.forEach(annuncio => {
-            
-        let colonna = document.createElement("div");
-
-        colonna.classList.add("col-4");
-
-        colonna.innerHTML = `
-        
-        <div class="card" >
-         <img src="${annuncio.immagine}" class="card-img-top" alt="foto villa">
-         <div class="card-body">
-           <h5 class="card-title">${annuncio.Regione}</h5>
-           <p class="card-text">
-           Euro ${annuncio.prezzo}
-           Metri quadri: ${annuncio.metri_quadrati}
-           </p>
-           
-         </div>
-        
-        `;
-
-        cont_annunci.appendChild(colonna);
-
-
+    data.forEach(annuncio => {
+      
+      let colonna = document.createElement("div");
+      
+      colonna.classList.add("col-4");
+      
+      colonna.innerHTML = `
+      
+      <div class="card" >
+      <img src="${annuncio.immagine}" class="card-img-top" alt="foto villa">
+      <div class="card-body">
+      <h5 class="card-title">${annuncio.Regione}</h5>
+      <p class="card-text">
+      Euro ${annuncio.prezzo}
+      Metri quadri: ${annuncio.metri_quadrati}
+      </p>
+      
+      </div>
+      
+      `;
+      
+      cont_annunci.appendChild(colonna);
+      
+      
     }); //End foreach
+    
+   
+    
+    
+  }
 
+  function creaRadioButton() {
+      
+    data.forEach(radioButton => {
+      
+      let cont_radio = document.createElement("div");
+      
+      cont_radio.classList.add("col-12");
+      
+      cont_radio.innerHTML = `
+      
+      <div class="form-check">
+      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+      <label class="form-check-label" for="flexRadioDefault1">
+      ${radioButton.Regione}
+      </label>
+      </div>
+              
+      `;
+      
+      contenitore_radio.appendChild(cont_radio);
 
-    }
+    })
+    
+  }
+  
+  creaCardAnnunci();
 
-    creaCardAnnunci();
-
-})
+  creaRadioButton();
+  
+});
 
