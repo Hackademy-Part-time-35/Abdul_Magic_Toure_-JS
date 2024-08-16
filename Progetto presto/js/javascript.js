50802bc4a976410c6bbbe159a2c094765442bff3
magic.toure <magictoureabdul@gmail.com>
2024-08-16 21:00:12 +0200

    versione revisionata e migliorata

--- Progetto presto/js/javascript.js
+++ Progetto presto/js/javascript.js
@@ -1,33 +1,16 @@
 
+let number1 = document.querySelector('#number1');
+let number2 = document.querySelector('#number2');
+let number3 = document.querySelector('#number3');
 
-let number1 = document.querySelector("#number1");
-
-let number2 = document.querySelector("#number2");
-
-let number3 = document.querySelector("#number3");
-
-let counter1 = 0;
-
-let counter2 = 0;
-
-let counter3 = 0;
-
-let intervallo1 = setInterval(() => {
-
-    if (counter1 < 200) {
-        counter1++;
-        number1.innerText = counter1;
-    } else {
-        clearInterval(intervallo1);
-    }
-
-}, 50)
+// let counter1 = 0;
+// let counter2 = 0;
+// let counter3 = 0;
 
 
 function crea_intervallo(elemento, numMax, tempo) {
-
     let counter = 0;
-    let intervallo = setInterval(() => {
+    let intervallo = setInterval(()=> {
 
         if (counter < numMax) {
             counter++;
@@ -36,65 +19,63 @@ function crea_intervallo(elemento, numMax, tempo) {
             clearInterval(intervallo);
         }
     
-    }, tempo)
+    }, tempo);
 
 }
 
-let controllo_ripetizione = false;
+let controllo_ripetzione = false;
 
 let osserva = new IntersectionObserver((entries) => {
 
     entries.forEach((entry) => {
 
-        if (entry.isIntersecting && controllo_ripetizione==false) {
+        if (entry.isIntersecting && controllo_ripetzione == false) {
 
             crea_intervallo(number1, 200, 50);
-            crea_intervallo(number2, 1200, 50);
-            crea_intervallo(number3, 200, 50);
+            crea_intervallo(number2, 1200, 5);
+            crea_intervallo(number3, 500, 10);
 
-            controllo_ripetizione = true;
+            controllo_ripetzione = true;
+           
 
-    //         let intervallo1 = setInterval(() => {
+           /*  let intervallo1 = setInterval(()=> {
 
-    //             if (counter1 < 200) {
-    //                 counter1++;
-    //                 number1.innerText = counter1;
-    //             } else {
-    //                 clearInterval(intervallo1);
-    //             }
+                if (counter1 < 200) {
+                    counter1++;
+                    number1.innerText = counter1;
+                } else {
+                    clearInterval(intervallo1);
+                }
             
-    //         }, 50)
+            }, 50);
 
-    //         let intervallo2 = setInterval(() => {
+            let intervallo2 = setInterval(()=> { 
 
-    //             if (counter2 < 1200) {
-    //                 counter2++;
-    //                 number2.innerText = counter2;
-    //             } else {
-    //                 clearInterval(intervallo2);
-    //             }
+                if (counter2 < 1200) {
+                    counter2++;
+                    number2.innerText = counter2;
+                } else {
+                    clearInterval(intervallo2);
+                }
             
-    //         }, 5)
+            }, 5)
 
-    //         let intervallo3 = setInterval(() => {
+            let intervallo3 = setInterval(()=> { 
 
-    //             if (counter3 < 500) {
-    //                 counter3++;
-    //                 number3.innerText = counter3;
-    //             } else {
-    //                 clearInterval(intervallo3);
-    //             }
+                if (counter3 < 500) {
+                    counter3++;
+                    number3.innerText = counter3;
+                } else {
+                    clearInterval(intervallo3);
+                }
             
-    //         }, 10)
+            }, 10)
+ */
 
-     }
+        }
 
- })
+    })
 
-})
-
-
+});
 
 osserva.observe(number3)
-
-//abbiamo creato l'oggetto osserva che ci permette alla visione di un determinato elemnto di eseguire una stringa di codice. usiamo IntersectionObserver, per applicare la prima parte di funzionalità, e all'interno delle parentesi tonde inseriamo "entries" che risulta essere un array, dunque possiamo usare le proprietà degli array ed usiamo ForEach per ciclare tutti gli elementi presenti all'interno dell'array, e decidiamo dunque di selezionare lo specifico elemento. Lo specifico elmento è definito da "entry". ed usiamo isIntersecting per la seconda parte di funzionalità che fa eseguire il codice qunado la sua condizione diventa TRUE. la parte finale necessità di essere richiamata con il nome dell'oggetto con .observe() e all'interno delle parentesi metto l'elmento che voglio che triggheri la mia funzione.
